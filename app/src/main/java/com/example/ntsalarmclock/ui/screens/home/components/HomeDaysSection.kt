5e6da770655bf1d94cfcfbf0d3d120cc3745133d
Alexandre Roux <alexandre.roux@ensc.fr>
2026-03-04 16:12:14 +0100

    Refactor home screen UI components and layout

--- app/src/main/java/com/example/ntsalarmclock/ui/screens/home/components/HomeDaysSection.kt
+++ app/src/main/java/com/example/ntsalarmclock/ui/screens/home/components/HomeDaysSection.kt
@@ -1,15 +1,11 @@
 package com.example.ntsalarmclock.ui.screens.home.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.example.ntsalarmclock.R
 import com.example.ntsalarmclock.ui.components.DayOfWeekUi
 import com.example.ntsalarmclock.ui.components.DaysOfWeekRow
@@ -24,8 +20,6 @@ fun HomeDaysSection(
             text = stringResource(R.string.days),
             style = MaterialTheme.typography.headlineLarge
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         DaysOfWeekRow(
             selectedDays = selectedDays,
---
+++ app/src/main/java/com/example/ntsalarmclock/ui/screens/home/components/HomeProgressiveVolumeSection.kt
@@ -0,0 +1,36 @@
+package com.example.ntsalarmclock.ui.screens.home.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.ntsalarmclock.R
+import com.example.ntsalarmclock.ui.screens.home.HomeScreenUiState
+
+@Composable
+fun HomeProgressiveVolumeSection(
+    state: HomeScreenUiState.Success,
+    onProgressiveVolumeEnabledChange: (Boolean) -> Unit
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = stringResource(R.string.progressive_volume),
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Text(
+            text = stringResource(R.string.progressive_volume_description),
+            color = MaterialTheme.colorScheme.secondary,
+            textAlign = TextAlign.Center,
+        )
+        Switch(
+            checked = state.progressiveVolume,
+            onCheckedChange = {
+                onProgressiveVolumeEnabledChange(it)
+            }
+        )
+    }
+}
